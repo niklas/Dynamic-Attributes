b8c37ed65ac63388c15e7270966b57f0def4ff50
Niklas Hofer <niklas+dev@lanpartei.de>
2010-07-01 12:54:53 +0200

    install rspec

---
+++ config/initializers/rspec_generator.rb
@@ -0,0 +1,6 @@
+DynamicAttributes::Application.configure do
+  config.generators do |g|
+    g.integration_tool :rspec
+    g.test_framework   :rspec
+  end
+end if defined? DynamicAttributes::Application
